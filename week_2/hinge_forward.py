eea8b133aa464ed9093da7bd295a83d47f6f7062
Marc Vornetran <marc.vornetran@gmail.com>
2017-11-07 22:17:18 +0100

    Use Jop's NumPy solution for hinge_forward

--- week_2/hinge_forward.py
+++ week_2/hinge_forward.py
@@ -1,5 +1,5 @@
-# TODO: Rewrite using NumPy functions
 def hinge_forward(target_pred, target_true):
+    """
     sum = 0;
 
     for pred_row, true_row in zip(target_pred, target_true):
@@ -7,5 +7,12 @@ def hinge_forward(target_pred, target_true):
             sum += np.maximum(0, 1 - pred * truth)
 
     output = sum / len(target_pred)
+    """
+
+    # Thanks Jop.
+    prod = np.multiply(target_pred, target_true)
+    maxFunc = np.maximum(0, (1 - prod))
+    totalSum = np.sum(maxFunc)
+    output = totalSum / len(target_pred)
 
     return output
