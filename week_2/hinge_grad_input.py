cbcb4386822782ae2c64ce34676ea94b56289160
Marc Vornetran <marc.vornetran@gmail.com>
2017-11-10 16:52:27 +0100

    Fix hinge_grad_input

--- week_2/hinge_grad_input.py
+++ week_2/hinge_grad_input.py
@@ -1,2 +1,2 @@
 def hinge_grad_input(target_pred, target_true):
-    return np.negative(target_true) / len(target_true)
+    return np.where(target_pred * target_true < 1, 1 / len(target_pred) * -target_true, 0)
