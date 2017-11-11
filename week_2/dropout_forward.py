466fd8e12ffffb27e6bf11f983cdca5d1113ddf8
Marc Vornetran <marc.vornetran@gmail.com>
2017-11-11 13:35:46 +0100

    Fix dropout_forward

--- week_2/dropout_forward.py
+++ week_2/dropout_forward.py
@@ -2,4 +2,4 @@ def dropout_forward(x_input, mask, drop_rate, training_phase):
     if training_phase:
         return x_input * mask
     else:
-        return x_input
+        return x_input * (1 - drop_rate)
