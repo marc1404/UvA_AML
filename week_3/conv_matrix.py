37a6fce10890596590145cd8a7c0c8e011a02c25
Marc Vornetran <marc.vornetran@gmail.com>
2017-11-14 23:28:12 +0100

    Refactor conv_matrix with the help of Jop

--- week_3/conv_matrix.py
+++ week_3/conv_matrix.py
@@ -2,18 +2,13 @@ def conv_matrix(matrix, kernel):
     output = np.ndarray(shape=matrix.shape)
     matrix_height, matrix_width = matrix.shape
     kernel_height, kernel_width = kernel.shape
-    x_offset = kernel_width // 2
-    y_offset = kernel_height // 2
-    padding = np.maximum(x_offset, y_offset)
-    padded = np.pad(matrix, padding, 'constant', constant_values=0)
+    padding_x = kernel_width // 2
+    padding_y = kernel_height // 2
+    padded = np.pad(matrix, [(padding_y, padding_y), (padding_x, padding_x)], 'constant', constant_values=0)
 
     for y in range(matrix_height):
         for x in range(matrix_width):
-            start_x = x + padding - x_offset
-            start_y = y + padding - y_offset
-            end_x = x + padding + 1 + x_offset
-            end_y = y + padding + 1 + y_offset
-            batch = padded[start_y:end_y, start_x:end_x]
+            batch = padded[y:y + kernel_height, x:x + kernel_width]
             multiplied = batch * kernel
             output[y][x] = np.sum(multiplied)
 
