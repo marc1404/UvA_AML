959f82a4b85654827520549f8ea66ade70f6219c
Marc Vornetran <marc.vornetran@gmail.com>
2017-11-14 18:32:46 +0100

    Tidy up convolutional matrix

--- week_3/conv_matrix.py
+++ week_3/conv_matrix.py
@@ -1,13 +1,14 @@
 def conv_matrix(matrix, kernel):
     output = np.ndarray(shape=matrix.shape)
+    matrix_height, matrix_width = matrix.shape
     kernel_height, kernel_width = kernel.shape
     x_offset = kernel_width // 2
     y_offset = kernel_height // 2
     padding = np.maximum(x_offset, y_offset)
     padded = np.pad(matrix, padding, 'constant', constant_values=0)
 
-    for (y, row) in enumerate(matrix):
-        for (x, value) in enumerate(row):
+    for y in range(matrix_height):
+        for x in range(matrix_width):
             start_x = x + padding - x_offset
             start_y = y + padding - y_offset
             end_x = x + padding + 1 + x_offset
